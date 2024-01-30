4593d4453c4e3935eec99cdeead60b71ad0e8f67
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2024-01-31 00:14:19 +0100

    Router , switch and link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 
 import './App.css';
+import Create from './Create';
 import Navbar  from './Navbar';
+import PostDetails from './PostDetails';
 import Home from './home';
+import { BrowserRouter as Router , Route, Switch} from 'react-router-dom/cjs/react-router-dom.min';
+
 export default function  App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className='content'>
-       <Home />
-
-     </div>   
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/create">
+            <Create />
+          </Route>
+          <Route path="/post/:id">
+            <PostDetails />
+          </Route>
+        </Switch>
+      </div>   
+      </div>
+    </Router>
   );
 }
 
