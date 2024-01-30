586607f1f5b5652336097070475221277d3be2f2
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2024-01-30 20:11:25 +0100

    before fetching data from json server

--- src/home.js
+++ src/home.js
@@ -1,32 +1,42 @@
 
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import Post from "./post";
 const Home = () => {
 
-     const [blogs, setBlog]= useState([
-        {title:'My new page', body:'lorem ipsum .....', author:'bara' , id:'1' },
-        {title:'Welcom to the party', body:'lorem ipsum .....', author:'bara', id:'2'  },
-        {title:'Web dev top Tips ', body:'lorem ipsum .....', author:'mahmoud', id:'3'  },
-        {title:'Yaeh yeah Salim in the House !!', body:'lorem ipsum .....', author:'Salim', id:'4'  },
-    ]) 
-  
+    const [blogs, setBlog]= useState([null]);
     const handelDelete= (id)=>{
         const newBlogs = blogs.filter((item)=>(
             item.id !==id 
                ))
                setBlog(newBlogs)
-        
-    }
+    };
+
+
+
+
+
+    useEffect(()=>{
+        fetch('http://localhost:8080/blogs').then(
+            res=> {
+                res.json()
+            }
+        ).then(data=>{
+            console.log(data)
+        })
+    }, [])
+
+    
     return (  
         <>
         <div className="home">
-        
         </div>
         <br/>
         <Post blogs={blogs} title="All Blogs !" handelDelete={handelDelete} />
-        <Post blogs={blogs.filter((item)=>(
+    {/*     <button onClick={()=>(setName('lugi'))}>Change Name</button>
+        <p>{name}</p> */}
+    {/*     <Post blogs={blogs.filter((item)=>(
             item.author ==='bara'
-          ))} title="Bara Blogs !" />
+          ))} title="Bara Blogs !" /> */}
         </>
 
         
@@ -46,3 +56,17 @@ export default Home;
       <button type="submit" onClick={handelClick}> fire </button>
             <button type="submit" onClick={()=> handelClickAgain('mario') }> fire Me again </button>
 */
+
+/*
+
+/* 
+    const [name, setName]= useState('bara')
+
+   useEffect(()=>{
+     console.log(name)
+     console.log("use Effect in action !!!")
+     /* localStorage.setItem('fuck', 'ffff')
+     const localStorageVariables= localStorage.getItem('fuck');
+     console.log(localStorageVariables) 
+     console.log(name)
+    }, [name]); */
