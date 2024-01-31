c4a51d7d1f661cbdb59c878ccbc45ca917897175
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2024-01-31 11:31:01 +0100

    delete a post

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,14 +1,25 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 const PostDetails = () => {
+    const history =useHistory();
     const {id} =useParams()
     const {data: singlePost, isPending, error} = useFetch(`http://localhost:8080/blogs/`+id)
 console.log(singlePost)
+const handelClick=()=>{
+    fetch('http://localhost:8080/blogs/'+id,{
+        method: 'DELETE',
+    }).then(()=>{
+        history.push('/')
+    }
+       
+    )
+}
     return ( 
 
         
         <div className="post-details">
-                        <h1>Post Details - {id} </h1>
+            <h1>Post Details - {id} </h1>
             <br></br><br></br><br></br>
             {isPending &&  <div>Loading ...</div>}
             {error && <div>{error}</div>}
@@ -18,7 +29,7 @@ console.log(singlePost)
                     <p>Written by : {singlePost.author}</p>
                     <div>{singlePost.body}</div>
 
-
+<button onClick={handelClick} className="delete-button">Delete</button>
                 </article>
                 </div>}
 
