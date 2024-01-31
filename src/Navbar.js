8f8e461ef84f9828326eaa795e4ebadc3346b7db
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2024-01-31 11:22:19 +0100

    navbar styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Timoumi Blog</h1>
             <div className="links">
-                <a href="/index.js">Home</a>
-                <a href="/creat" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     fontSize: '15px',
                     borderRadius: '8px'
-                }}>Create</a>
+                }}>Create</Link>
             </div>
         </nav>
 
